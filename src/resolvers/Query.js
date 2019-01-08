5f03e089b0fde3414e3765fd63c96e343c35cca4
Jackson Ricardo Schroeder <jacksons@fesctecnologia.com.br>
2019-01-08 08:45:29 -0200

    query filter support

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,11 +1,22 @@
-const feed = (root, args, { prisma }) => prisma.links();
-const users = (root, args, { prisma }) => prisma.users();
-const user = (root, { id }, { prisma }) => prisma.user({ id });
-const link = (root, { id }, { prisma }) => prisma.link({ id });
+const feed = async (root, { filter }, { prisma }) => {
+  const where = filter
+    ? {
+        OR: [
+          { description_contains: filter },
+          { url_contains: filter },
+        ],
+      }
+    : {};
+  
+  return await prisma.links({ where })
+}
+const users = (root, args, { prisma }) => prisma.users()
+const user = (root, { id }, { prisma }) => prisma.user({ id })
+const link = (root, { id }, { prisma }) => prisma.link({ id })
 
 module.exports = {
   feed,
   users,
   user,
-  link,
+  link
 };
